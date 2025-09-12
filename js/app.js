d226a6ca1bb25b06d89dc7bfa53fb36d8a43b424
Luiz Lins <luizlins@MacBook-Air-de-Luiz.local>
2025-09-12 19:43:14 -0300

    criando fotografias e criando lista

--- js/app.js
+++ js/app.js
@@ -1,14 +1,21 @@
 
-async function usuarios() {
-    const response = fetch('https://jsonplaceholder.typicode.com/users')
+async function getURI(uri) {
+    const response = fetch(`https://jsonplaceholder.typicode.com/${uri}`)
         .then(response => response.json())
     return response
 }
 
 async function listarUsuarios() {
-    const data = await usuarios()
+    const data = await usuarios('/users')
     data.forEach(item => {
         criarElementoLi(item.name)
+    });
+}
+
+async function listarFotografias() {
+    const data = await getURI('/photos')
+    data.forEach(item => {
+        criarImagem(item.thumbnailUrl)
     });
 }
 
@@ -19,5 +26,12 @@ function criarElementoLi(contexto) {
     listaNaoOrdenada.appendChild(elementLi)
 }
 
+function criarImagem(img) {
+    const elementLi = document.createElement('li')
+    elementLi.innerHTML = `<img src='${img}'/>`
+    const listaNaoOrdenada = document.querySelector('ul')
+    listaNaoOrdenada.appendChild(elementLi)
+}
+
 const button = document.querySelector('button')
-button.addEventListener('click', () => listarUsuarios())
+button.addEventListener('click', () => listarFotografias())
