002a68ffc455c6bb46dedd729d16dc3e218b0b13
Luiz Lins <luizlins@MacBook-Air-de-Luiz.local>
2025-09-17 19:28:42 -0300

    Criando uma task dinamica

--- js/app.js
+++ js/app.js
@@ -1,37 +1,23 @@
 
-async function getURI(uri) {
-    const response = fetch(`https://jsonplaceholder.typicode.com/${uri}`)
-        .then(response => response.json())
-    return response
+
+const capturandoTarefa = () => {
+    const tarefa = document.getElementById('tarefa')
+    const aux = tarefa.value 
+    tarefa.value = ''
+    return aux
 }
 
-async function listarUsuarios() {
-    const data = await usuarios('/users')
-    data.forEach(item => {
-        criarElementoLi(item.name)
-    });
+const elementoCriarTarefa = (tarefa) => {
+    const listaTarefas = document.getElementById('lista-tarefas')
+    const elementoButton = document.createElement('button')
+    elementoButton.classList.add('list-group-item')
+    elementoButton.classList.add('list-group-item-action')
+    elementoButton.innerText = tarefa
+    listaTarefas.appendChild(elementoButton)
 }
 
-async function listarFotografias() {
-    const data = await getURI('/photos')
-    data.forEach(item => {
-        criarImagem(item.thumbnailUrl)
-    });
-}
-
-function criarElementoLi(contexto) {
-    const elementLi = document.createElement('li')
-    elementLi.textContent = contexto
-    const listaNaoOrdenada = document.querySelector('ul')
-    listaNaoOrdenada.appendChild(elementLi)
-}
-
-function criarImagem(img) {
-    const elementLi = document.createElement('li')
-    elementLi.innerHTML = `<img src='${img}'/>`
-    const listaNaoOrdenada = document.querySelector('ul')
-    listaNaoOrdenada.appendChild(elementLi)
-}
-
-const button = document.querySelector('button')
-button.addEventListener('click', () => listarFotografias())
+const botaoEnviarTarefa = document.getElementById('butao-enviar-tarefa')
+botaoEnviarTarefa.addEventListener('click', (e) => {
+    const textoTarefa = capturandoTarefa()
+    elementoCriarTarefa(textoTarefa)
+})
